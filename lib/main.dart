f1fd5ccfee979936b35f783435ed7885c4cc1d3c
Ronan Pelot <ronanpelot@gmail.com>
2026-04-18 19:28:17 -0400

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:api_quiz_app/quiz_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz Game',
+      home: const QuizScreen(),
+    );
+  }
+}
